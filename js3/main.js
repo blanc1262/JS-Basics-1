a16b280925f747f4f08fdcb76b6b526ebfaaa25b
Jplantar <jeraldplantar4@gmail.com>
2025-03-04 19:21:44 +0800

    JS Basics

--- js3/main.js
+++ js3/main.js
@@ -1,4 +1,4 @@
-const numInput = document.getElementById("num1");
+const numInput = document.getElementById("num");
 const elementTxt = document.getElementById("element");
 const sumTxt = document.getElementById("sum");
 const highTxt = document.getElementById("high");
@@ -6,25 +6,44 @@ const lowTxt = document.getElementById("low");
 const insertBtn = document.getElementById("insertBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 
-let numbers = [];
+let array = [];
 
-insertBtn.onclick = () => {
+insertBtn.onclick = function() {
     let num = parseInt(numInput.value);
-    if (!isNaN(num)) {
-        numbers.push(num);
+    if (num == num) { 
+        array.push(num);
         updateDisplay();
-        numInput.value = "";
-    }
+    } else
+        alert("tarunga uy")
+    numInput.value = ""; 
 };
 
-deleteBtn.onclick = () => {
-    numbers = [];
+deleteBtn.onclick = function() {
+    array = [];
     updateDisplay();
 };
 
 function updateDisplay() {
-    elementTxt.innerHTML = numbers.join("<br>") || "-";
-    sumTxt.innerHTML = numbers.reduce((a, b) => a + b, 0);
-    highTxt.innerHTML = numbers.length ? Math.max(...numbers) : "-";
-    lowTxt.innerHTML = numbers.length ? Math.min(...numbers) : "-";
+    elementTxt.innerHTML = "";
+    sumTxt.innerHTML = "";
+    highTxt.innerHTML = "";
+    lowTxt.innerHTML = "";
+
+    if (array.length > 0) {
+        elementTxt.innerHTML = "";
+        let sum = 0;
+        let highest = array[0];
+        let lowest = array[0];
+
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+            if (array[i] > highest) highest = array[i];
+            if (array[i] < lowest) lowest = array[i];
+            elementTxt.innerHTML += array[i] + "<br>";
+        }
+
+        sumTxt.innerHTML = sum;
+        highTxt.innerHTML = highest;
+        lowTxt.innerHTML = lowest;
+    }
 }
